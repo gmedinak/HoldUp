b326db1082bf5c5980bc06a45c320894c0f2532c
csimps04 <32078201+csimps04@users.noreply.github.com>
2019-06-05 21:03:05 -0700

    updated import list

--- vectorization.py
+++ vectorization.py
@@ -1,5 +1,25 @@
 import warnings
+import pandas as pd
+import numpy as np
+import pickle
+import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.stem.porter import *
+import string
+import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as VS
+from textstat.textstat import *
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import classification_report
+from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
+import seaborn
 
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -7,30 +27,47 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Functions for feature extraction and learning process
 
-def import_from_csv(data_location)
+def import_from_csv(data_location):
    csv_data = pd.read_csv(data_location) 
    return data
    
    
 
-def get_wordFreq_vector()
+def get_wordFreq_vector():
    
    pass
    
 
-def get_tf_vector(input)
+def get_tf_vector(input):
    tf_vector = tf_vectorizor.fit_transform(input).toarray()
    return tf_vector
    
-def get_features(csv_data)
+def get_features(csv_data):
    tweets = csv_data.tweet
+   pass
    
-   
+def get_pos_tfidf_vector(tweet_tags):
+   pos_vectorizer = TfidfVectorizer(
+    tokenizer=None,
+    lowercase=False,
+    preprocessor=None,
+    ngram_range=(1, 3),
+    stop_words=None,
+    use_idf=False,
+    smooth_idf=False,
+    norm=None,
+    decode_error='replace',
+    max_features=5000,
+    min_df=5,
+    max_df=0.75,
+    )
+    
+    pos = pos_vectorizer.fit_transform(
    
    
 #expects the tf vector as input, preprocess should be a defined function 
 #  
-def get_tfid_vector(tweets, tf_vector, tokenize, preprocess)
+def get_tfid_vector(tweets, tf_vector, tokenize, preprocess):
    
    tfidf_vectorizer = TfidfVectorizer(
     tokenizer=tokenize,
@@ -53,7 +90,7 @@ def get_tfid_vector(tweets, tf_vector, tokenize, preprocess)
     
    pass
    
-def get_pos_vector(tweets, preprocess, basic_tokenize)
+def get_pos_vector(tweets, preprocess, basic_tokenize):
    tweet_tags = []
    
    for tweet in tweets:
@@ -67,14 +104,27 @@ def get_pos_vector(tweets, preprocess, basic_tokenize)
       
    pass
    
-def get_training_features()
+def get_pos_vector_spacy(tweets):
+   tokens = []
+   tweet_tags = []
+   docs = []
+   
+   for tweet in tweets
+      doc = nlp(tweet)
+      docs.append(doc)
+         
+      tag_list = [x.tag_ for x in doc]
+      tag_str = "".join(tag_list)
+      tweet_tags.append(tag_str)
+   
+def get_training_features():
    vectorizer = CountVectorizer(stor_words = "english",
                                 preprocessor = clean_text)
    training_features = vectorizer.fit_transform(train_data["text"])
    pass
    
    
-def get_SVM_model(training_features, train_data)
+def get_SVM_model(training_features, train_data):
    model = LinearSVC()
    model.fit(training_features, train_data["setiment"])
    return model
