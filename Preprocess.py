d57c704d9ae2068b21ddda6b8ba344fb8e5fb9ef
csimps04 <32078201+csimps04@users.noreply.github.com>
2019-06-05 20:48:34 -0700

    Added some of the basics for pre-learning step
    vectorization holds the functions that create the feature vectors (POS,  Tf/IDF) and preprocess holds the functions that get called in the vectorization file

---
+++ Preprocess.py
@@ -0,0 +1,43 @@
+
+
+stopwords=stopwords = nltk.corpus.stopwords.words("english")
+
+other_exclusions = ["#ff", "ff", "rt"]
+stopwords.extend(other_exclusions)
+
+stemmer = PorterStemmer()
+
+def tokenize_nltk(tweet):
+   tokens = nltk.word_tokenize(tweet)
+   return tokens
+
+# Tokenize and converts each token into word stem
+# Example of this - processing -> process
+def tokenize_stemming(tweet)
+   tweet_t = " ".join(re.split("[^a-zA-Z]*\s", tweet.lower())).strip()
+   tokens = [stemmer.stem(t) for t in tweet_t.split()]
+   return tokens
+   
+# No stemming
+def tokenize_no_stemming(tweet):
+   tweet_t = " ".join(re.split("[^a-zA-Z.,!?]*", tweet.lower())).strip()
+   return tweet.split()
+   
+def preprocess(text_string):
+   """
+   Accepts a text string and replaces:
+   1) urls with URLHERE
+   2) lots of whitespace with one instance
+   3) mentions with MENTIONHERE
+
+   This allows us to get standardized counts of urls and mentions
+   Without caring about specific people mentioned
+   """
+   space_pattern = '\s+'
+   giant_url_regex = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'
+       '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+   mention_regex = '@[\w\-]+'
+   parsed_text = re.sub(space_pattern, ' ', text_string)
+   parsed_text = re.sub(giant_url_regex, '', parsed_text)
+   parsed_text = re.sub(mention_regex, '', parsed_text)
+   return parsed_text
--- vectorization.py
+++ vectorization.py
@@ -1,2 +1,85 @@
-def do_vectorization():
-    pass
+import warnings
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+
+# Functions for feature extraction and learning process
+
+def import_from_csv(data_location)
+   csv_data = pd.read_csv(data_location) 
+   return data
+   
+   
+
+def get_wordFreq_vector()
+   
+   pass
+   
+
+def get_tf_vector(input)
+   tf_vector = tf_vectorizor.fit_transform(input).toarray()
+   return tf_vector
+   
+def get_features(csv_data)
+   tweets = csv_data.tweet
+   
+   
+   
+   
+#expects the tf vector as input, preprocess should be a defined function 
+#  
+def get_tfid_vector(tweets, tf_vector, tokenize, preprocess)
+   
+   tfidf_vectorizer = TfidfVectorizer(
+    tokenizer=tokenize,
+    preprocessor=preprocess,
+    ngram_range=(1, 3),
+    stop_words=stopwords,
+    use_idf=True,
+    smooth_idf=False,
+    norm=None,
+    decode_error='replace',
+    max_features=10000,
+    min_df=5,
+    max_df=0.75
+    )
+    
+   tfidf = tfidf_vectorizer.fit_transform(tweets).to_array()
+   vocab = {v:i for i, v in enumerate(vectorizer.get_feature_names())}
+   idf_values = vectorizer.idf_
+   idf_dict = {i:idf_values[i] for i in vocab.values()}
+    
+   pass
+   
+def get_pos_vector(tweets, preprocess, basic_tokenize)
+   tweet_tags = []
+   
+   for tweet in tweets:
+      tokens = basic_tokenize(preprocess(tweet))
+      tags = nltk.pos_tag(tokens)
+      tag_list = [x[1] for x in tags]
+      tag_str = " ".join(tag_list)
+      tweet_tags.append(tag_str)
+   
+   return 
+      
+   pass
+   
+def get_training_features()
+   vectorizer = CountVectorizer(stor_words = "english",
+                                preprocessor = clean_text)
+   training_features = vectorizer.fit_transform(train_data["text"])
+   pass
+   
+   
+def get_SVM_model(training_features, train_data)
+   model = LinearSVC()
+   model.fit(training_features, train_data["setiment"])
+   return model
+   pass
+   
+def get_ngram_model()
+   
+   pass
